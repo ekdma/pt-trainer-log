d0dea56e5b115939e0875843fdcf83c05dc8da08
ekdma <seoo4009@gmail.com>
2025-07-26 00:53:21 +0900

    분할 추가

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -5,13 +5,9 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 import { CheckIcon } from 'lucide-react'
 import { cn } from '@/lib/utils' // Tailwind class merge 유틸 함수 (없으면 classNames 써도 됨)
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface CheckboxProps
-  extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {}
-
 export const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
-  CheckboxProps
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
 >(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
