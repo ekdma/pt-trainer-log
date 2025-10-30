e59700d233170482a83e716296ac8493457198a1
ekdma <seoo4009@gmail.com>
2025-10-30 13:54:23 +0900

    로그인 화면 레이아웃 수정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect, useRef } from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { getSupabaseClient } from '@/lib/supabase'
 import { useLanguage } from '@/context/LanguageContext'
